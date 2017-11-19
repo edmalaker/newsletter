3bd19d461b6c59fb5d2a7192d6164ad78f7ceb74
Ed Malaker <edmalaker@gmail.com>
2017-11-19 16:39:33 -0500

    Add files via upload

--- form.php
+++ form.php
@@ -1,10 +1,10 @@
 <form method="post" class="form1" action="formhandler1.php">
 
     <label class="form1-field" for="name">Name:</label>
-		<input type="text" name="name" class="form1-control" >
+		<input type="text" name="name" class="form1-control" value="Anonymous" size="15" maxlength="15" required />
 		
     <label class="form1-field" for="email">Email:</label>
-		<input type="email" name="email" class="form1-control"/>
+		<input type="email" name="email" class="form1-control" size="15" maxlength="30" required />
 
     <button class="form1-field" type="submit" name="submit" value ="Sign Up" class="form1-button">Submit</button>
 
